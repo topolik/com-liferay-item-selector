d01f62e0718d1bb0edec863bcc195c23fa38db9e
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-02-19 15:47:36 -0800

    LPS-61099 Move to "apps/collaboration"

---
+++ item-selector-taglib/src/main/resources/META-INF/resources/config.js
@@ -0,0 +1,26 @@
+;(function() {
+	var PATH_ITEM_SELECTOR_TAGLIB = Liferay.ThemeDisplay.getPathContext() + '/o/item-selector-taglib';
+
+	AUI().applyConfig(
+		{
+			groups: {
+				'item-selector-taglib': {
+					base: PATH_ITEM_SELECTOR_TAGLIB + '/',
+					combine: Liferay.AUI.getCombine(),
+					modules: {
+						'liferay-image-selector': {
+							path: 'image_selector/js/image_selector.js',
+							requires: [
+								'aui-base',
+								'liferay-item-selector-dialog',
+								'liferay-portlet-base',
+								'uploader'
+							]
+						}
+					},
+					root: PATH_ITEM_SELECTOR_TAGLIB + '/'
+				}
+			}
+		}
+	);
+})();
